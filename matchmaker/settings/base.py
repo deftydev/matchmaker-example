f8f47cec748d2088acc060eb94858bc2a02a4e57
devansh gupta <58273709+deftydev@users.noreply.github.com>
2020-05-30 17:34:25 +0530

    Update base.py

--- matchmaker/settings/base.py
+++ matchmaker/settings/base.py
@@ -28,8 +28,8 @@ DEBUG = False
 ALLOWED_HOSTS = []
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'devanshgupta79212346@gmail.com'
-EMAIL_HOST_PASSWORD = 'Devanshggg99274'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
